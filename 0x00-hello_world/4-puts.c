33539e3503022507cd7e15258f3010f1c51e00e8
Edom Tsegaye <linyetsegesh@gmail.com>
2022-07-07 10:34:37 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main -entry point 
+ * main - entry point 
  * 
  * Return: always 0 (correct)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
